6533b42883c43cbf8de575ae96139b81a71ddca5
UiSiongKhu <uisiong.khu@gmail.com>
2025-08-05 20:46:21 +0800

    Kā Rhyme API ê GET hong hoat kái chò RESTful API ê hêng sek.

--- src/routes/rhymesRoute.ts
+++ src/routes/rhymesRoute.ts
@@ -3,7 +3,7 @@ import { getRhyme, initRhymes } from "../controllers/rhymeController";
 
 const router = Router();
 
-router.get('/get', getRhyme);
-router.get('/init', initRhymes);
+router.get('/', getRhyme); // Read
+router.post('/init', initRhymes); // Create
 
 export default router;
